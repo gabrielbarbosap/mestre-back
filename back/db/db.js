bb1951f0ffbaa12833ace86af5889e24acda48f2
Gabriel Barbosa Pereira <gabriel.pereira@meta.com.br>
2021-05-12 19:21:55 -0300

    eroku

--- back/db/db.js
+++ back/db/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose')
 
-const url = `mongodb+srv://entededor:rafa121297@cluster0.v9wtq.mongodb.net/entededor?retryWrites=true&w=majority`;
-
-const connectionParams = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}
-mongoose.connect(url, connectionParams)
+mongoose.connect(
+        process.env.MONGO_URL_CONNECT, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        }
+    )
     .then(() => {
         console.log('Connected to database ')
     })
